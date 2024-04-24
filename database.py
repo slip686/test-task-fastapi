6d11bcfef528e9b62e33a5b48de04979a3daf646
Kirill <slip686@gmail.com>
2024-04-24 11:30:46 +0300

    automatic table creation added

--- database.py
+++ database.py
@@ -3,10 +3,16 @@ from sqlalchemy import create_engine
 from settings import get_settings
 from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine
 from sqlalchemy.orm import sessionmaker
+from models.models import Base, TrackPointModel, VehicleModel
 
 config = get_settings()
 engine = AsyncEngine(create_engine(config.db.dsn, echo=True, pool_pre_ping=True))
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def init_models():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
 
 async def get_session() -> AsyncSession:
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
 
+from database import init_models
 from routers.vehicle import vehicle_router
-
 
 app = FastAPI(title="TEST-TASK")
 app.include_router(vehicle_router, tags=["vehicles"])
 
+
+@app.on_event("startup")
+async def on_startup():
+    await init_models()
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
